0f587c7b6d838ed27f40819a7c0110c3c414788d
amanverma685 <av36232@gmail.com>
2023-04-18 23:36:18 +0530

    task

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -7,7 +7,7 @@ terraform {
     secret_key = "ZmN2/lqymp4jIVRAUjRQNqYZGZYRTf5LkmL4uZwx"
     region     = "ap-south-1"
   }
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -37,15 +37,15 @@ module "authorisation_api" {
   env                   = "dev"
   host                  = "vt-survey-database.copaymbev82s.us-east-2.rds.amazonaws.com"
   database              = "vt_survey_management"
-  psycopg2_arn          = "arn:aws:lambda:us-east-2:124017527459:layer:psycopg2_layer_for_python_3_8:1"
+  psycopg2_arn          = "arn:aws:lambda:ap-south-1:834781518900:layer:psycopg2_layer_for_python_3_8:1"
   user                  = "postgres"
   password              = "admin123"
   port                  = "5432"
-  user_pool_arn         = "arn:aws:cognito-idp:us-east-2:124017527459:userpool/us-east-2_QODZHpxZr"
-  client_id             = "144rrggh9ivjcdooa1nu45sa5s"
+  user_pool_arn         = "arn:aws:cognito-idp:ap-south-1:834781518900:userpool/ap-south-1_cKnp6b0v0"
+  client_id             = "41ktatknk96rrk9kjgpe6ighdc"
   group_name_requester  = "requester"
   group_name_respondent = "respondent"
-  user_pool_id          = "us-east-2_QODZHpxZr"
+  user_pool_id          = "ap-south-1_cKnp6b0v0"
 }
 
 # module "survey_management_apis" {
