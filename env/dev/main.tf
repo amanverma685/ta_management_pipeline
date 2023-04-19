e3876fd761ddd94f154e9dcf9f80d99774a0f374
amanverma685 <av36232@gmail.com>
2023-04-19 16:17:44 +0530

    chnges

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -24,13 +24,6 @@ provider "aws" {
   secret_key = "ZmN2/lqymp4jIVRAUjRQNqYZGZYRTf5LkmL4uZwx"
 }
 
-# module "cognito" {
-#   source = "../../cognito/"
-# }
-
-# module "aws_rds_instance" {
-#     source ="../../rds_db_instance/"
-# }
 
 module "authorisation_api" {
   source                = "../../authorisation_apis/"
@@ -48,49 +41,6 @@ module "authorisation_api" {
   user_pool_id          = "ap-south-1_cKnp6b0v0"
 }
 
-# module "survey_management_apis" {
-#   source        = "../../survey_management_apis/"
-#   env           = "dev"
-#   host          = "vt-survey-database.copaymbev82s.us-east-2.rds.amazonaws.com"
-#   database      = "vt_survey_management"
-#   psycopg2_arn  = "arn:aws:lambda:us-east-2:124017527459:layer:psycopg2_layer_for_python_3_8:1"
-#   user          = "postgres"
-#   password      = "admin123"
-#   port          = "5432"
-#   user_pool_arn = "arn:aws:cognito-idp:us-east-2:124017527459:userpool/us-east-2_QODZHpxZr"
-#   user_pool_id  = "us-east-2_QODZHpxZr"
-# }
 
-# module "mdm_apis" {
-#   source = "../../mdm_apis"
-# }
 
-# module "survey_search_apis" {
-#   source = "../../survey_search_apis"
-# }
 
-# module "survey_metrics_api" {
-#   source        = "../../get_survey_metrics/"
-#   env           = "dev"
-#   host          = "vt-survey-database.copaymbev82s.us-east-2.rds.amazonaws.com"
-#   database      = "vt_survey_management"
-#   psycopg2_arn  = "arn:aws:lambda:us-east-2:124017527459:layer:psycopg2_layer_for_python_3_8:1"
-#   user          = "postgres"
-#   password      = "admin123"
-#   port          = "5432"
-#   user_pool_arn = "arn:aws:cognito-idp:us-east-2:124017527459:userpool/us-east-2_QODZHpxZr"
-#   user_pool_id  = "us-east-2_QODZHpxZr"
-# }
-
-# module "requester_management_api" {
-#   source        = "../../requester_management_api/"
-#   env           = "dev"
-#   host          = "vt-survey-database.copaymbev82s.us-east-2.rds.amazonaws.com"
-#   database      = "vt_survey_management"
-#   psycopg2_arn  = "arn:aws:lambda:us-east-2:124017527459:layer:psycopg2_layer_for_python_3_8:1"
-#   user          = "postgres"
-#   password      = "admin123"
-#   port          = "5432"
-#   user_pool_arn = "arn:aws:cognito-idp:us-east-2:124017527459:userpool/us-east-2_QODZHpxZr"
-#   user_pool_id  = "us-east-2_QODZHpxZr"
-# }
