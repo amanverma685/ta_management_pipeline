772e8d53e4e3ce65c2159f222552332bc3ad9388
AmanV <av36232@gmail.com>
2023-08-07 14:52:44 +0530

    Update main.tf

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket = "ta-management-deployment"
     key = "prod"
-    access_key = "AKIA4EXG3DQ2JCNNGCCF"
-    secret_key = "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"
+    access_key = "**"
+    secret_key = "***"
     region = "ap-south-1"
   }
   required_providers {
@@ -18,8 +18,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA4EXG3DQ2JCNNGCCF"
-  secret_key = "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"
+  access_key = "**"
+  secret_key = "***"
 }
 
 module "authorisation_api" {
@@ -28,8 +28,8 @@ module "authorisation_api" {
   host                  = "spe-major-project.cqfn3y5ohgqj.ap-south-1.rds.amazonaws.com"
   database              = "spe_major"
   psycopg2_arn          = "arn:aws:lambda:us-east-1:834781518900:layer:psycopg2:1"
-  user                  = "postgres"
-  password              = "admin123"
+  user                  = "*****"
+  password              = "****"
   port                  = "5432"
   user_pool_arn         = "arn:aws:cognito-idp:ap-south-1:834781518900:userpool/ap-south-1_cKnp6b0v0"
   client_id             = "41ktatknk96rrk9kjgpe6ighdc"
@@ -45,8 +45,8 @@ module "is_user_registered" {
   host                  = "spe-major-project.cqfn3y5ohgqj.ap-south-1.rds.amazonaws.com"
   database              = "spe_major"
   psycopg2_arn          = "arn:aws:lambda:us-east-1:834781518900:layer:psycopg2:1"
-  user                  = "postgres"
-  password              = "admin123"
+  user                  = "*****"
+  password              = "****"
   port                  = "5432"
   user_pool_arn         = "arn:aws:cognito-idp:ap-south-1:834781518900:userpool/ap-south-1_cKnp6b0v0"
   user_pool_id          = "ap-south-1_cKnp6b0v0"
